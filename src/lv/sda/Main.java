a21d89224a8c036ed4c936f4e10c42e0343affa3
Dasha Yurchenko <dasha.yurchenko@inbox.lv>
2021-07-08 21:47:17 +0300

    Initial commit

---
+++ src/lv/sda/Main.java
@@ -0,0 +1,23 @@
+package lv.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1.setAge(29);
+        person1.setGender("Female");
+        person1.setHeight(178);
+        person1.setComplexion("White");
+
+        System.out.println(person1.getAge());
+        System.out.println(person1.getComplexion());
+        System.out.println(person1.getGender());
+        System.out.println(person1.getHeight());
+        person1.running();
+        person1.eating();
+        person1.walking();
+        person1.talking();
+
+    }
+}
---
+++ src/lv/sda/Person.java
@@ -0,0 +1,56 @@
+package lv.sda;
+
+public class Person {
+    private double height;
+    private String gender;
+    private String complexion;
+    private int age;
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public String getComplexion(){
+        return complexion;
+    }
+
+    public void setComplexion(String complexion) {
+        this.complexion = complexion;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void walking(){
+        System.out.println("The person is walking near to me!");
+    }
+
+    public void talking(){
+        System.out.println("The person is talking to me!");
+    }
+
+    public void eating(){
+        System.out.println("The person is eating my food!");
+    }
+
+    public void running(){
+        System.out.println("The person is running from me!");
+    }
+}
